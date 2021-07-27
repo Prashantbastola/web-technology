335ab990ad7bbb253128d2e09c3f054866728b49
Prashantbastola <81133692+Prashantbastola@users.noreply.github.com>
2021-07-27 11:59:02 +0545

    Create dashboard.php

---
+++ lab9/dashboard.php
@@ -0,0 +1,13 @@
+<?php
+require_once("./sql/connect.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>redirecting</title>
+</head>
+<body style="text-align: center, background color:purple;">
+	<p>This is dashboard</p>
+	
+</body>
+</html>
