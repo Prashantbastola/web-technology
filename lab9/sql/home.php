742599b05c0a9e5089d33a48b1e4a0e611582e59
Prashantbastola <81133692+Prashantbastola@users.noreply.github.com>
2021-07-27 12:20:07 +0545

    Create home.php

---
+++ lab9/sql/home.php
@@ -0,0 +1,23 @@
+  
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		body{
+			text-align: center;
+			border:1px;
+			background-color: #224455;
+		}
+		#bo{
+			color: white;
+			text-decoration: none;
+		}
+	</style>
+</head>
+<body>
+	<h2><a id="bo" href="./start/validate.php">validate</a></h2>
+	<h2><a id="bo" href="./login.php">Login</a></h2>
+	<h2><a id="bo" href="./register.php">Sign up</a></h2>
+</body>
+</html>
